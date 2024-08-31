96c2567bd16c99e119f778ed9c373f796d04cc22
TechNinja-01 <soni.harsh.3234@gmail.com>
2024-08-31 11:35:01 +0530

    Second Commit

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -14,18 +14,21 @@ const home = async (req, res) => {
       console.log(req.body);
       const { username, email, phone, password } = req.body;
   
-      const userExist = await User.findOne({ email: email });
+      const userExist = await User.findOne({ email });
   
       if (userExist) {
         return res.status(400).json({ msg: "Email already exists" });
       }
+      
+      
+      const userCreated = await User.create({ username, email, phone, password: hash_password });
+      
   
-      const userCreated = await User.create({ username, email, phone, password });
-  
-      // res.status(201).json({ message: "User registered successfully" });
+    
       res.status(201).json({ msg: userCreated });
+      
     } catch (error) {
-      res.status(500).json({ message: "Internal server error" });
+      res.status(500).json({ message: "Internal Server Error:" + error.message });
     }
   };
   
--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -21,9 +23,27 @@ const userSchema = new mongoose.Schema({
         default:false,
     },
 
-})
+});
 
   const User = new mongoose.model("User", userSchema);
+
+//   secure the password with bcrypt
+userSchema.pre("save", async function(){
+    const user = this;
+
+    if(!user.isModified("Password")) {
+        next();
+    }
+
+    try {
+      const saltRound = await bcryt.gensalt(10);
+      const hash_password = await bcryt.hash(user.password, saltRound);
+      user.password = hash_password;
+    } catch (error) {
+        next(error);
+    }
+
+});
   
 //   const testDocument = new ExampleModel({name: 'Harsh', age: 20});
 //   testDocument.save().then(() => console.log('Document saved successfully'));
